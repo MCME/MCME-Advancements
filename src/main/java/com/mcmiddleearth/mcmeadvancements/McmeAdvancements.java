9c7b9065f78512bc73d48f7739f20d20fe024767
EriolEandur <Eriol_Eandur@gmx.de>
2023-12-26 22:19:51 +0100

    plugin setup

---
+++ src/main/java/com/mcmiddleearth/mcmeadvancements/McmeAdvancements.java
@@ -0,0 +1,17 @@
+package com.mcmiddleearth.mcmeadvancements;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class McmeAdvancements extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
